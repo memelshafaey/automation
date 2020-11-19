5f866023319f6752ec71eb4063f177d9a83b731e
memelshafaey <memelshafaey@gmail.com>
2020-11-19 17:10:04 +0200

    .

--- src/test/java/com/araby/utils/Constant.java
+++ src/test/java/com/araby/utils/Constant.java
@@ -19,8 +19,8 @@ public class Constant {
 	public static final String DEFAULT_SENDER = EG_WE_SENDER;
 	
 	public static final long TEST_TIMEOUT = 600000;
-	//public static final String DEFAULT_LANDING = "https://staging-uri.araby.deals/ar-EG/";
-	public static final String DEFAULT_LANDING = "https://www.google.com/";
+	public static final String DEFAULT_LANDING = "https://staging-uri.araby.deals/ar-EG/";
+	//public static final String DEFAULT_LANDING = "https://www.google.com/";
 	public static final int TEST_INTERVAL = 10000;
 
 }
